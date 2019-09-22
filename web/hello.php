901ebe4bef797bb860c010eecc7c4275d80d23c8
Mark Tobler <marktobler@gmail.com>
2019-09-21 23:58:10 -0700

    added header and html tags

--- web/hello.php
+++ web/hello.php
@@ -1,8 +1,11 @@
 <?php
 
-  echo '&lt;div style=&quot;color:orange;&quot;&rt;Hello World!&lt;&frasl;div&rt;';
+  #echo '&lt;div style=&quot;color:orange;&quot;&rt;Hello World!&lt;&frasl;div&rt;';
 
   #echo '&lt;div style="color:orange;"&rt;';
   #echo 'Hello World!';
   #echo '&lt;/div&rt;';
+  echo '<html><head></head><body>';
+  echo '<div style="color:orange;">Hello World! </div>';
+  echo '</body></html>';
 ?>
