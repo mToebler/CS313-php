f19c9685975a8c53310a64fc901aee0b098cb067
Mark Tobler <marktobler@gmail.com>
2019-09-21 23:53:59 -0700

    changed to a php extension.

---
+++ web/hello.php
@@ -0,0 +1,8 @@
+<?php
+
+  echo '&lt;div style=&quot;color:orange;&quot;&rt;Hello World!&lt;&frasl;div&rt;';
+
+  #echo '&lt;div style="color:orange;"&rt;';
+  #echo 'Hello World!';
+  #echo '&lt;/div&rt;';
+?>
